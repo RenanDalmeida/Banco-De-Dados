f068df31a9a7d0951e641c09a0eaff248e4ebbbf
RenanDalmeida <rdalmeidi@outlook.com>
2020-12-01 20:57:11 -0300

    DQL Optus finalizado

---
+++ Atividades/Pets-Optus/DQL-Optus.sql
@@ -0,0 +1,11 @@
+--DQL
+
+USE optus;
+
+SELECT * FROM Estilo;
+
+SELECT * FROM ESTILO WHERE Nome = 'Rap';
+
+SELECT * FROM Artista WHERE Nome LIKE '%li%' OR Nome LIKE 'k%';
+
+SELECT * FROM Artista;
---
+++ Atividades/Pets-Optus/DQL-Pets.sql
@@ -0,0 +1,14 @@
+--DQL
+
+USE clinica;
+
+
+SELECT * FROM Veterinario ORDER BY Nome ASC;
+
+SELECT * FROM Pet ORDER BY Nome;
+
+SELECT * FROM Raca WHERE Descricao LIKE '%dle';
+
+SELECT * FROM Raca;
+
+SELECT * FROM Veterinario;
